ce12b4847117e7b44573a2db6c5d947a7ac84d4f
lemarc <lemarc.dev@gmail.com>
2024-09-03 11:25:11 +0200

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,22 @@
-import { resolve } from "node:path";
-import { defineConfig } from "vite";
+import { resolve } from 'node:path';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   build: {
     lib: {
-      entry: resolve(__dirname, "src/main.js"),
-      name: "QuazalaRedisConnector",
+      entry: resolve(__dirname, 'src/main.js'),
+      name: 'QuazalaRedisConnector',
       fileName: (format) => `main.${format}.js`,
-      formats: ["es", "cjs"],
+      formats: ['es', 'cjs'],
     },
     rollupOptions: {
-      external: [/^node:/, "events", "stream", "path"],
+      external: [/^node:/, 'events', 'stream', 'path'],
     },
-    target: "node18",
+    target: 'node18',
     minify: false,
   },
   test: {
     globals: true,
-    environment: "node",
+    environment: 'node',
   },
 });
